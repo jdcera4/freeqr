8bd1dbec1da84fe7e5dfa8d97eb328f5e7eda016
jdcera4 <jdcera4@gmail.com>
2022-03-27 09:27:21 -0500

    updated files

--- initAuth.js
+++ initAuth.js
@@ -1,47 +1,21 @@
-import { init } from 'next-firebase-auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const initAuth = ()=> {
-  init({
-    authPageURL: '/auth',
-    appPageURL: '/',
-    loginAPIEndpoint: '/api/login', // required
-    logoutAPIEndpoint: '/api/logout', // required
-    firebaseAdminInitConfig: {
-      credential: {
-        projectId: 'freeqr-1e734',
-        clientEmail: 'firebase-adminsdk-gjzsa@freeqr-1e734.iam.gserviceaccount.com',
-        // The private key must not be accessible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      },
-      databaseURL: 'https://my-example-app.firebaseio.com',
-      storageBucket: "freeqr-1e734.appspot.com",
-    },
-    firebaseClientInitConfig: {
-      apiKey: "AIzaSyBFZWWs4Hc2FAj-AtF4s4gESbE7Ef7gKFg",
-      authDomain: "freeqr-1e734.firebaseapp.com",
-      projectId: "freeqr-1e734",
-      storageBucket: "freeqr-1e734.appspot.com",
-      messagingSenderId: "858040272157",
-      appId: "1:858040272157:web:9132bdd651257184e84df4",
-      measurementId: "G-SNMKPJFWXT"
-    },
-    cookies: {
-      name: 'freeqr-app', // required
-      // Keys are required unless you set `signed` to `false`.
-      // The keys cannot be accessible on the client side.
-      keys: [
-        process.env.COOKIE_SECRET_CURRENT,
-        process.env.COOKIE_SECRET_PREVIOUS,
-      ],
-      httpOnly: true,
-      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
-      overwrite: true,
-      path: '/',
-      sameSite: 'strict',
-      secure: true, // set this to false in local (non-HTTPS) development
-      signed: true,
-    },
-  })
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFZWWs4Hc2FAj-AtF4s4gESbE7Ef7gKFg",
+  authDomain: "freeqr-1e734.firebaseapp.com",
+  projectId: "freeqr-1e734",
+  storageBucket: "freeqr-1e734.appspot.com",
+  messagingSenderId: "858040272157",
+  appId: "1:858040272157:web:9132bdd651257184e84df4",
+  measurementId: "G-SNMKPJFWXT"
+};
 
-export default initAuth
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
